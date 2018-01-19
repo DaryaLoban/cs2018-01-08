f9acd5f2a383ab71091b647521f47045c0e26f9e
DaryaLoban <daryalobanhere@gmail.com>
2018-01-19 12:27:27 +0300

    Done Lesson02:A123,B123,C123 Lesson03:A12,B12,C12 Lesson04:A123,B123,C123 Lesson05:A12,B12,C12 Lesson06:A12
    ToDo
    Lesson02:-
    Lesson03:-
    Lesson04:-
    Lesson05:-
    Lesson06:B1,C1

--- src/by/it/daryaloban/lesson06/TaskA2.java
+++ src/by/it/daryaloban/lesson06/TaskA2.java
@@ -25,5 +25,16 @@ package by.it.daryaloban.lesson06;
 
 */
 public class TaskA2 {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        Dog d2 = new Dog();
+        d1.setName("Шарик");
+        d1.setAge(5);
+        d2.setName("Тузик");
+        d2.setAge(3);
+
+        System.out.println(d1);
+        System.out.println(d2);
+    }
 
 }
