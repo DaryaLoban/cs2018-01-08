f51fecb411743b48a3a81e9b1d8612298c07bc6e
oymarygold <msMaog@gmail.com>
2018-01-21 18:01:36 +0300

    Доделанные задания

--- src/by/it/oymarygold/lesson05/TaskB1.java
+++ src/by/it/oymarygold/lesson05/TaskB1.java
@@ -10,6 +10,23 @@ package by.it.oymarygold.lesson05;
 */
 
 
-public class TaskB1 {
+import java.util.ArrayList;
 
+public class TaskB1 {
+    public static void main(String[] args)
+            throws Exception {
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add ("мама");
+        list.add("папа");
+        list.add("бабушка");
+        list.add("дедушка");
+        list.add("сестра");
+
+        System.out.println(list.size());
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(list.get(i));
+        }
+    }
 }
