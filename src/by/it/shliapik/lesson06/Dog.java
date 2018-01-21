e6746c6f2ecc65a395c2684d73a5db9c7cd5d419
shliapik <nat-kka@mail.ru>
2018-01-21 16:51:49 +0300

    Lesson06 all tasks are completed

---
+++ src/by/it/shliapik/lesson06/Dog.java
@@ -0,0 +1,61 @@
+package by.it.shliapik.lesson06;
+
+public class Dog {
+    private String name;
+    private int age;
+    private int weight; //вес собаки
+    private double power; //сила укуса собаки
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Кличка: " + name + ". Возраст: "+age;
+    }
+    boolean win(Dog otherDog) {
+        double chanceThis=0.2*this.age+0.3*this.weight+0.5*this.power;
+        double chanceOther=0.2*otherDog.age+0.3*otherDog.weight+0.5*otherDog.power;
+        if (chanceThis>chanceOther) return true;
+        else return false;
+    }
+}
+
